aed8573c556f6358e85b9a332839ed0a9066b34d
Franklyn-Ukwueze <franklynifechukwu2@gmail.com>
2024-06-18 07:52:09 +0100

    studied django syntax

--- first_project/members/views.py
+++ first_project/members/views.py
@@ -25,9 +25,10 @@ def main(request):
     return HttpResponse(template.render())
 
 def testing(request):
+    mymembers = Member.objects.all().values()
     template = loader.get_template('template.html')
     context = {
-        'fruits': ['Apple', 'Banana', 'Cherry'],
+        'fruits': ['Banana', 'Orange', 'Apple'],
     }
 
     return HttpResponse(template.render(context, request))
